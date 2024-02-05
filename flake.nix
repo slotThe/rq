4770ca516932710d1881f5b063d05dd72aba455a
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-02-05 18:46:51 +0100

    Add nix package

--- flake.nix
+++ flake.nix
@@ -1,25 +1,45 @@
 {
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows     = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let overlays = [ (import rust-overlay) ];
-          pkgs     = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [
-            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
-            rust-analyzer
-          ];
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        toolchain =
+          pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+        platform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
         };
-      }
-    );
+        cargoMeta =
+          (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+      in with pkgs; {
+        packages = {
+          rq = platform.buildRustPackage {
+            pname = "rq";
+            inherit (cargoMeta) version;
+            src = ./.;
+            meta = {
+              license = lib.licenses.gpl3;
+              description =
+                "a tiny functional language with which you can manipulate JSON";
+            };
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          default = self.packages."${system}".rq;
+        };
+
+        devShells.default =
+          mkShell { buildInputs = [ toolchain rust-analyzer ]; };
+      });
 }
