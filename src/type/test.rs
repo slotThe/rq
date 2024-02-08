c815b6b9267b066d4fbb85570db10e1eaafa3d48
Tony Zorman <soliditsallgood@mailbox.org>
2024-02-08 09:02:46 +0100

    type/test: Add check macro

--- src/type/test.rs
+++ src/type/test.rs
@@ -1,22 +1,36 @@
 use std::collections::BTreeMap;
 
-use crate::{expr::{de_bruijn::DBVar, parser::parse}, r#type::{arr, checker::TypeCheckError, TVar, Type}};
+use crate::{eval::stdlib::STDLIB_TYPES, expr::{de_bruijn::DBVar, parser::parse}, r#type::{arr, checker::TypeCheckError, TVar, Type}};
+
+macro_rules! check_eq {
+  ($left:expr, $right:expr $(,)?) => {
+    assert_eq!(
+      &parse($left).unwrap().infer(&STDLIB_TYPES),
+      &Ok($right)
+    )
+  };
+  ($left:expr, $right:expr, $($arg:tt)+) => {
+    assert_eq!(
+      &parse($left).unwrap().infer(&STDLIB_TYPES),
+      &Ok($right),
+      $($arg)+
+    )
+  };
+}
 
 #[test]
 #[allow(non_snake_case)]
 fn infer_type_of_S_combinator() {
   use Type::*;
-  let expr = parse("λf -> λg → |x| f x (g x)").unwrap();
-  let typ = expr.infer(&BTreeMap::new());
-  assert_eq!(
-    typ,
-    Ok(arr(
+  check_eq!(
+    "λf -> λg → |x| f x (g x)",
+    arr(
       arr(Var(TVar(0)), arr(Var(TVar(1)), Var(TVar(2)))),
       arr(
         arr(Var(TVar(0)), Var(TVar(1))),
         arr(Var(TVar(0)), Var(TVar(2))),
       )
-    ))
+    )
   );
 }
 
